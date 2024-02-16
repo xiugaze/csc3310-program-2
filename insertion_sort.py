aeb01071843c25a8411b3bd9f3c2920f1dc91c75
Wesley Jochman <jochmanw@msoe.edu>
2024-02-16 11:35:24 -0600

    Create insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def sort(input):
+    for i in range(1, len(input)):
+        key = input[i]
+        j = i - 1
+        while j >= 0 and key < input[j]:
+            input[j + 1] = input[j]
+            j -= 1
+        input[j + 1] = key
+    return input
